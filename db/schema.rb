fb5a33fd85cedf3840a91a5cf313719e19f3ef52
mthorry <mthorry@gmail.com>
2017-08-15 09:32:59 -0400

    Done.

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 20170814220258) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "released"
+    t.integer  "release_year"
+    t.string   "artist_name"
+    t.string   "genre"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+end
